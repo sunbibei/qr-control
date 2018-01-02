824eccd187f5bb95f86e33c316aaf3dec3c29b9c
Bibei <sunbibei@sina.com>
2018-01-03 00:29:01 +0800

    add rock gait

---
+++ src/gait/rock/rock.cpp
@@ -0,0 +1,25 @@
+/*
+ * rock.cpp
+ *
+ *  Created on: Jan 3, 2018
+ *      Author: bibei
+ */
+
+#include "gait/rock/rock.h"
+
+namespace qr_control {
+
+struct RockParams {
+	;
+};
+
+Rock::Rock() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Rock::~Rock() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace qr_control */
